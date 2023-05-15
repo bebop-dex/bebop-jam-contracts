6d0a75892d633929732c7fe35861352bb2c73a4a
monokh <mnokhb@gmail.com>
2023-05-15 23:59:48 +0100

    openzeppelin

--- src/Token.sol
+++ src/Token.sol
@@ -1,3 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor() ERC20("Token", "TOK") {
+
+    }
+}
